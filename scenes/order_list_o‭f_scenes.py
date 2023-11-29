dcf7f76c1de3452077cbc829c475719e61e0a085
Jean Allen <jean.allen@geog.ucsb.edu>
2023-11-29 10:26:09 -0800

    keeping things up to date

---
+++ scenes/order_list_o‭f_scenes.py
@@ -0,0 +1,11 @@
+import datetime
+import json
+import os
+import requests
+import time
+from requests.auth import HTTPBasicAuth
+
+search_output = ' fill this in @jean '
+
+
+df['Image_IDs'].to_list()
